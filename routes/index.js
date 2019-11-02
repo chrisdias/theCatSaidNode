2a15d643bac322afee840b092b6aa40e8c71af60
chrisdias <cdias@microsoft.com>
2019-11-01 18:06:29 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var fs = require('fs');
+require('dotenv').config()
+
+router.get('/', function (req, res, next) {
+
+  myPlatform = process.platform
+
+  let inImgPath = "/images/catpaw.png";
+
+  if (myPlatform != "linux") {
+    inImgPath = "/images/catpaw.png"
+  }
+
+  res.render('index', 
+  { 
+    title: "The Cat Said No!",
+    imgPath: inImgPath
+  }
+  );
+});
+
+module.exports = router;
