ba828b89813c1e120cc66b36cdcbc41cda0a910e
chrisdias <cdias@microsoft.com>
2020-04-02 15:27:26 -0700

    3 spaces!

--- app.js
+++ app.js
@@ -22,36 +22,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true
+   secret: 'secret',
+   resave: false,
+   saveUninitialized: true
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // res.locals is an object passed to hbs engine
-app.use(function(req, res, next) {
-    res.locals.session = req.session;
-    next();
+app.use(function (req, res, next) {
+   res.locals.session = req.session;
+   next();
 });
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err['status'] = 404;
-  next(err);
+app.use(function (req, res, next) {
+   var err = new Error('Not Found');
+   err['status'] = 404;
+   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,20 @@ require('dotenv').config()
 
 router.get('/', function (req, res, next) {
 
-  myPlatform = process.platform
+   myPlatform = process.platform
 
-  let inImgPath = "/images/catpaw.png";
+   let inImgPath = "/images/catpaw.png";
 
-  if (myPlatform != "linux") {
-    inImgPath = "/images/catpaw.png"
-  }
+   if (myPlatform != "linux") {
+      inImgPath = "/images/catpaw.png"
+   }
 
-  res.render('index', 
-  { 
-    title: "The Cat Said No!",
-    imgPath: inImgPath
-  }
-  );
+   res.render('index',
+      {
+         title: "The Cat Said No!",
+         imgPath: inImgPath
+      }
+   );
 });
 
 module.exports = router;
