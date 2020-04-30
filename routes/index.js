f2c0c2518015e740f6f4377b3530d0f41717770a
chrisdias <cdias@microsoft.com>
2020-04-30 16:09:38 -0700

    remove this for the demo!! Fixes #9

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 router.get('/', function (req, res, next) {
 
-   myPlatform = process.platform
+   //myPlatform = process.platform
 
    let inImgPath = "/images/catpaw.png";
 
