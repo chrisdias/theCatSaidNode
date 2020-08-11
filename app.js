bf4024bca70b38952950ae9c631bc11203c5e44a
Chris Dias <cdias@microsoft.com>
2020-08-11 14:05:27 -0700

    chhhhhangessss

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ var index = require('./routes/index');
 
 var app = express();
 
-// this is going to github
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
